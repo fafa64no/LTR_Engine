3111ac815a26dea46a80dc369c03d8a616e65df7
fafa64no <148809822+fafa64no@users.noreply.github.com>
2023-10-25 02:01:50 +0200

    base stuff

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-clang src/main.cpp -LTR_Engine.exe
+libs=-luser32
+warnings=-Wno-writable-strings
+
+clang src/main.cpp -LTR_Engine.exe $libs $warnings
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,66 @@
 
 
+
+// ############################################################################
+//                            Platform Globals
+// ############################################################################
 static bool running=true;
 
+// ############################################################################
+//                            Platform Functions
+// ############################################################################
+bool platform_create_window(int width, int height, char* title);
+
+// ############################################################################
+//                            Windows Platform
+// ############################################################################
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+
+// ############################################################################
+//                            Windows Globals
+// ############################################################################
+
+// ############################################################################
+//                            Platform Implementations
+// ############################################################################
+bool platform_create_window(int width, int height, char* title){
+    HINSTANCE instance=GetModuleHandleW(0);
+    WNDCLASSW wc={};
+    wc.hIcon            =LoadIcon(instance, IDI_APPLICATION);
+    wc.hCursor          =LoadCursor(NULL,IDC_ARROW);
+    wc.lpszClassName    =(LPCWSTR)title;
+    wc.lpfnWndProc      =DefWindowProcW;
+    if (!RegisterClassW(&wc)){
+        return false;
+    }
+    int dwStyle=WS_OVERLAPPEDWINDOW;
+    HWND window=CreateWindowExW(
+        0,
+        (LPCWSTR)title,
+        (LPCWSTR)title,
+        dwStyle,
+        100,
+        100,
+        width,
+        height,
+        NULL,
+        NULL,
+        instance,
+        NULL
+    );
+    if (window==NULL){
+        return false;
+    }
+    ShowWindow(window, SW_SHOW);
+    return true;
+}
+#endif
+
 int main(){
+    platform_create_window(1200,720,"LTR Engine");
     while (running){
         //Do stuff
     }
