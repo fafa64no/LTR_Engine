433ea6e59552f027efc341317c0e012d908f7db6
fafa64no <148809822+fafa64no@users.noreply.github.com>
2024-06-24 19:54:42 +0200

    Folders

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ timestamp=$(date +%s)
 
 libs="-luser32 -lopengl32 -lgdi32 -lthird_party/Lib/freetype.lib"
 warnings="-Wno-writable-strings -Wno-format-security -Wno-deprecated-declarations -Wno-switch -Wno-unused-value"
-includes="-Ithird_party -Ithird_party/Include -Isrc -Iincludes -Ithird_party/freetype"
+includes="-Ithird_party -Ithird_party/Include -Ithird_party/freetype 
+-Isrc -Isrc/game -Isrc/physics -Isrc/platform -Isrc/rendering
+-Iincludes -Iincludes/game -Iincludes/physics -Iincludes/platform -Iincludes/rendering"
 
 clang++ $includes -g src/main.cpp -oLTR_Engine.exe $libs $warnings
